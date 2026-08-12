685cef2ab313b19fea738cc7e3f2a5623bc11be4
dashboardia <dashboardia10@gmail.com>
2026-08-11 22:37:23 -0300

    Corrige hidratação do dashboard
    * Corrige hidratação do dashboard
    
    * Corrige hidratação do dashboard

--- app/page.js
+++ app/page.js
@@ -8,16 +8,26 @@ import { getConfigurationStatus } from "../lib/env";
 
 export const dynamic = "force-dynamic";
 
+function getDateLabel() {
+  return new Intl.DateTimeFormat("pt-BR", {
+    weekday: "long",
+    day: "2-digit",
+    month: "long",
+    timeZone: "America/Sao_Paulo",
+  }).format(new Date()).toUpperCase();
+}
+
 export default async function Home() {
   const configuration = getConfigurationStatus();
+  const dateLabel = getDateLabel();
 
   if (!configuration.githubAuth || !configuration.database) {
-    return <Dashboard setupMode />;
+    return <Dashboard setupMode dateLabel={dateLabel} />;
   }
 
   const session = await getServerSession(authOptions);
   if (!session?.user) redirect("/login");
   const data = await getDashboardData(session.user);
 
-  return <Dashboard data={data} user={session.user} />;
+  return <Dashboard data={data} user={session.user} dateLabel={dateLabel} />;
 }
